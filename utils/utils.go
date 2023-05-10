2a87a33c9fb89903a427eff563b7f687f694dc55
ngenohkevin <ngenohkevin19@gmail.com>
2023-05-10 17:31:21 +0300

    comment on unused code

--- utils/utils.go
+++ utils/utils.go
@@ -19,44 +19,51 @@ type BlockonomicsPrice struct {
 	Price float64 `json:"price"`
 }
 
+type RateCache struct {
+	rate       float64
+	expiration time.Time
+}
+
+var cache RateCache
+
 func ParseFloat(s string) (float64, error) {
 	return strconv.ParseFloat(s, 64)
 }
 
-func GetBitcoinPrice() (float64, error) {
-	resp, err := http.Get(BlockonomicsRateApi)
-	if err != nil {
-		log.Printf("Error getting blockonomics rate: %s", err.Error())
-		return 0, err
-	}
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-
-		}
-	}(resp.Body)
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Printf("Error reading blockonomics rate response: %s", err.Error())
-		return 0, err
-	}
-
-	var blockonomicsPrice map[string]interface{}
-	err = json.Unmarshal(body, &blockonomicsPrice)
-	if err != nil {
-		log.Printf("Error unmarshaling blockonomics rate JSON: %s", err.Error())
-		return 0, err
-	}
-
-	bitcoinUSDPrice, ok := blockonomicsPrice["price"].(float64)
-	if !ok {
-		log.Printf("Error getting blockonomics rate: invalid response")
-		return 0, err
-	}
-
-	return bitcoinUSDPrice, nil
-}
+//func GetBitcoinPrice() (float64, error) {
+//	resp, err := http.Get(BlockonomicsRateApi)
+//	if err != nil {
+//		log.Printf("Error getting blockonomics rate: %s", err.Error())
+//		return 0, err
+//	}
+//	defer func(Body io.ReadCloser) {
+//		err := Body.Close()
+//		if err != nil {
+//
+//		}
+//	}(resp.Body)
+//
+//	body, err := io.ReadAll(resp.Body)
+//	if err != nil {
+//		log.Printf("Error reading blockonomics rate response: %s", err.Error())
+//		return 0, err
+//	}
+//
+//	var blockonomicsPrice map[string]interface{}
+//	err = json.Unmarshal(body, &blockonomicsPrice)
+//	if err != nil {
+//		log.Printf("Error unmarshaling blockonomics rate JSON: %s", err.Error())
+//		return 0, err
+//	}
+//
+//	bitcoinUSDPrice, ok := blockonomicsPrice["price"].(float64)
+//	if !ok {
+//		log.Printf("Error getting blockonomics rate: invalid response")
+//		return 0, err
+//	}
+//
+//	return bitcoinUSDPrice, nil
+//}
 
 func GetCurrentTime() time.Time {
 	return time.Now()
@@ -66,7 +73,12 @@ func GetExpiryTime() time.Time {
 	return time.Now().Add(15 * time.Minute)
 }
 
-func ConvertToBitcoinUSD(priceInUSD float64) (float64, error) {
+func getBlockonomicsRate() (float64, error) {
+	if cache.expiration.After(time.Now()) {
+		// Rate is still valid, return it from cache
+		return cache.rate, nil
+	}
+
 	resp, err := http.Get(BlockonomicsRateApi)
 	if err != nil {
 		log.Printf("Error getting blockonomics rate: %s", err.Error())
@@ -93,6 +105,20 @@ func ConvertToBitcoinUSD(priceInUSD float64) (float64, error) {
 	}
 
 	bitcoinUSDPrice := blockonomicsPrice.Price
+
+	// Cache the rate and its expiration time
+	cache.rate = bitcoinUSDPrice
+	cache.expiration = time.Now().Add(5 * time.Minute) // cache for 5 minutes
+
+	return bitcoinUSDPrice, nil
+}
+
+func ConvertToBitcoinUSD(priceInUSD float64) (float64, error) {
+	bitcoinUSDPrice, err := getBlockonomicsRate()
+	if err != nil {
+		return 0, err
+	}
+
 	bitcoinAmount := priceInUSD / bitcoinUSDPrice
 
 	return bitcoinAmount, nil
