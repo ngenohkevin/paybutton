01062f745e023a13998d266ef0bc67cfedd6273d
ngenohkevin <ngenohkevin19@gmail.com>
2023-05-19 15:15:53 +0300

    comment out

--- main.go
+++ main.go
@@ -86,62 +86,6 @@ func main() {
 		})
 
 	})
-	////callback handler not working. Needs to some refactoring
-	//
-	//r.POST("/callback", func(c *gin.Context) {
-	//	address := c.PostForm("address")
-	//	paidAmountStr := c.PostForm("paidAmount")
-	//	email := c.PostForm("email")
-	//
-	//	if address == "" || paidAmountStr == "" || email == "" {
-	//		c.JSON(http.StatusBadRequest, gin.H{
-	//			"message": "Invalid input: address, paidAmount, and email are required",
-	//		})
-	//		return
-	//	}
-	//
-	//	paidAmount, err := utils.ParseFloat(paidAmountStr)
-	//	if err != nil {
-	//		c.JSON(http.StatusBadRequest, gin.H{
-	//			"message": "Invalid input: paidAmount must be a valid number",
-	//		})
-	//		return
-	//	}
-	//
-	//	err = payments.MarkPaymentAsPaid(address, paidAmount, email)
-	//	if err != nil {
-	//		c.JSON(http.StatusInternalServerError, gin.H{
-	//			"message": fmt.Sprintf("Error marking payment as paid: %s", err.Error()),
-	//		})
-	//		return
-	//	}
-	//
-	//	// Open the file for appending
-	//	file, err := os.OpenFile("paid_emails.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	//	if err != nil {
-	//		c.JSON(http.StatusInternalServerError, gin.H{
-	//			"message": fmt.Sprintf("Error writing paid email to file: %s", err.Error()),
-	//		})
-	//		return
-	//	}
-	//	defer func(file *os.File) {
-	//		if err := file.Close(); err != nil {
-	//			fmt.Printf("Error closing file: %v", err)
-	//		}
-	//	}(file)
-	//
-	//	// Write the email address to the file
-	//	if _, err := file.WriteString(fmt.Sprintf("%s ---> %s\n", email, address)); err != nil {
-	//		c.JSON(http.StatusInternalServerError, gin.H{
-	//			"message": fmt.Sprintf("Error writing paid email to file: %s", err.Error()),
-	//		})
-	//		return
-	//	}
-	//
-	//	c.JSON(http.StatusOK, gin.H{
-	//		"message": fmt.Sprintf("Payment with address %s has been marked as paid. Email %s has been saved.", address, email),
-	//	})
-	//})
 
 	err := r.Run()
 	if err != nil {
