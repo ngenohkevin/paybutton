c0a97c2d0b466d76ffbcb634928ee6cda61f0354
ngenohkevin <ngenohkevin19@gmail.com>
2024-06-12 00:37:04 +0300

    dynamic address

--- main.go
+++ main.go
@@ -11,289 +11,165 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
-var botApiKey string
+var (
+	botApiKey     string
+	chatID        int64 = 6074038462
+	addressLimit        = 5              // Limit the number of addresses generated per user/session
+	addressExpiry       = 24 * time.Hour // Set address expiry time to 24 hours
+)
+
+type UserSession struct {
+	Email              string
+	GeneratedAddresses map[string]time.Time // Track generated addresses and their creation time
+}
+
+var userSessions = make(map[string]*UserSession)
 
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
-		return
 	}
 
 	botApiKey = os.Getenv("BOT_API_KEY")
+	if botApiKey == "" {
+		log.Fatal("BOT_API_KEY not set in .env file")
+	}
 
-	//Initialize Telegram Bot
 	bot, err := tgbotapi.NewBotAPI(botApiKey)
 	if err != nil {
 		log.Fatal(err)
 	}
-	//Set the chatID of the user where you want to send the message
-	chatID := int64(6074038462)
 
 	r := gin.Default()
 	r.Use(cors.Default())
 
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Payment Service API",
-		})
+		c.JSON(http.StatusOK, gin.H{"message": "Payment Service API"})
 	})
 
-	r.POST("/cards", func(c *gin.Context) {
-		//clientIP := "41.57.100.198"
-		clientIP := c.ClientIP()
-
-		//fetch data from ipAPI
-		ipAPIData, err := utils.GetIpLocation(clientIP)
-		if err != nil {
-			log.Printf("Error getting ip location: %s", err.Error())
-		}
-
-		email := c.PostForm("email")
-		priceStr := c.PostForm("price")
-		description := c.PostForm("description")
-		name := c.PostForm("name")
-
-		if email == "" || priceStr == "" {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"message": "Invalid input: email and price are required",
-			})
-			return
-		}
-
-		priceUSD, err := utils.ParseFloat(priceStr)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"message": "Invalid input: price must be a valid number",
-			})
-			return
-		}
-		priceBTC, err := utils.ConvertToBitcoinUSD(priceUSD)
-		address, err := payments.GenerateBitcoinAddress(email, priceBTC)
-
-		//sites address
-		//address := "bc1qgjnaesfp5k7s8sxz8mq7a3p8rzwpzr3wzp956s"
-
-		localTime, err := ipAPIData.ParseLocalTime()
-		if err != nil {
-			log.Printf("Error parsing local time: %s", err)
-			// Handle the error as needed
-		}
-		logMessage := fmt.Sprintf("Email: %s, Address: %s, Amount: %.2f, Name: %s, Product: %s", email, address, priceUSD, name, description)
-		log.Printf(logMessage)
-
-		//botLogMessage := fmt.Sprintf("*Email:* `%s`\n*Address:* `%s`\n*Amount:* `%0.2f`\n*Name:* "+
-		//	"`%s`\n*Product:* `%s`",
-		//	email, address, priceUSD, name, description)
-
-		botLogMessage := fmt.Sprintf("*Email:* `%s`\n*Address:* `%s`\n*Amount:* `%0.2f`\n*Name:* "+
-			"`%s`\n*Product:* `%s`\n*IP Address:* `%s`\n*Country:* `%s`\n*State:* `%s`\n*City:* `%s`\n*Local Time:* `%s`",
-			email, address, priceUSD, name, description, clientIP, ipAPIData.Location.Country, ipAPIData.Location.State, ipAPIData.Location.City, localTime)
-
-		msg := tgbotapi.NewMessage(chatID, botLogMessage)
-		msg.ParseMode = tgbotapi.ModeMarkdown
-		_, err = bot.Send(msg)
-		if err != nil {
-			log.Printf("Error sending message to user: %s", err.Error())
-		}
-
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"message": fmt.Sprintf("Error getting Bitcoin price: %s", err.Error()),
-			})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{
-			"address":     address,
-			"priceInUSD":  priceUSD,
-			"priceInBTC":  priceBTC,
-			"email":       email,
-			"created_at":  utils.GetCurrentTime(),
-			"expired_at":  utils.GetExpiryTime(),
-			"description": description,
-			"name":        name,
-		})
-
-	})
-
-	r.POST("/usdt", func(c *gin.Context) {
-		//clientIP := "41.57.100.198"
-		clientIP := c.ClientIP()
-
-		//fetch data from ipAPI
-		ipAPIData, err := utils.GetIpLocation(clientIP)
-		if err != nil {
-			log.Printf("Error getting ip location: %s", err.Error())
-		}
-
-		email := c.PostForm("email")
-		priceStr := c.PostForm("price")
-		description := c.PostForm("description")
-		name := c.PostForm("name")
-
-		if email == "" || priceStr == "" {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"message": "Invalid input: email and price are required",
-			})
-			return
-		}
-
-		priceUSD, err := utils.ParseFloat(priceStr)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"message": "Invalid input: price must be a valid number",
-			})
-			return
-		}
-		priceBTC, err := utils.ConvertToBitcoinUSD(priceUSD)
-		//address, err := payments.GenerateBitcoinAddress(email, priceBTC)
-
-		address := "TMm1VE3JhqDiKyMmizSkcUsx4i4LJkfq7G"
-
-		localTime, err := ipAPIData.ParseLocalTime()
-		if err != nil {
-			log.Printf("Error parsing local time: %s", err)
-			// Handle the error as needed
-		}
-		logMessage := fmt.Sprintf("Email: %s, Address: %s, Amount: %.2f, Name: %s, Product: %s", email, address, priceUSD, name, description)
-		log.Printf(logMessage)
-
-		//botLogMessage := fmt.Sprintf("*Email:* `%s`\n*Address:* `%s`\n*Amount:* `%0.2f`\n*Name:* "+
-		//	"`%s`\n*Product:* `%s`",
-		//	email, address, priceUSD, name, description)
-
-		botLogMessage := fmt.Sprintf("*Email:* `%s`\n*Address:* `%s`\n*Amount:* `%0.2f`\n*Name:* "+
-			"`%s`\n*Product:* `%s`\n*IP Address:* `%s`\n*Country:* `%s`\n*State:* `%s`\n*City:* `%s`\n*Local Time:* `%s`",
-			email, address, priceUSD, name, description, clientIP, ipAPIData.Location.Country, ipAPIData.Location.State, ipAPIData.Location.City, localTime)
-
-		msg := tgbotapi.NewMessage(chatID, botLogMessage)
-		msg.ParseMode = tgbotapi.ModeMarkdown
-		_, err = bot.Send(msg)
-		if err != nil {
-			log.Printf("Error sending message to user: %s", err.Error())
-		}
-
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"message": fmt.Sprintf("Error getting Bitcoin price: %s", err.Error()),
-			})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{
-			"address":     address,
-			"priceInUSD":  priceUSD,
-			"priceInBTC":  priceBTC,
-			"email":       email,
-			"created_at":  utils.GetCurrentTime(),
-			"expired_at":  utils.GetExpiryTime(),
-			"description": description,
-			"name":        name,
-		})
-	})
-
-	r.POST("/payment", func(c *gin.Context) {
-		//Get IP of the client
-		clientIP := c.ClientIP()
-
-		//fetch data from ipAPI
-		ipAPIData, err := utils.GetIpLocation(clientIP)
-		if err != nil {
-			log.Printf("Error getting ip location: %s", err.Error())
-		}
-
-		email := c.PostForm("email")
-		priceStr := c.PostForm("price")
-		description := c.PostForm("description")
-		name := c.PostForm("name")
-
-		if email == "" || priceStr == "" {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"message": "Invalid input: email and price are required",
-			})
-			return
-		}
-
-		priceUSD, err := utils.ParseFloat(priceStr)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{
-				"message": "Invalid input: price must be a valid number",
-			})
-			return
-		}
-		priceBTC, err := utils.ConvertToBitcoinUSD(priceUSD)
-		address, err := payments.GenerateBitcoinAddress(email, priceBTC)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"message": fmt.Sprintf("Error generating Bitcoin address: %s", err.Error()),
-			})
-			return
-		}
-		//comment GenerateQRCode when pushing to production
-		//ur, err := utils.GenerateBitcoinURI(address, priceBTC)
-		//if err != nil {
-		//	_ = fmt.Errorf("%v", err)
-		//}
-		localTime, err := ipAPIData.ParseLocalTime()
-		if err != nil {
-			log.Printf("Error parsing local time: %s", err)
-		}
-		logMessage := fmt.Sprintf("Email: %s, Address: %s, Amount: %.2f, Name: %s, Product: %s", email, address, priceUSD, name, description)
-		log.Printf(logMessage)
-
-		//botLogMessage := fmt.Sprintf("*Email:* `%s`\n*Address:* `%s`\n*Amount:* `%0.2f`\n*Name:* "+
-		//	"`%s`\n*Product:* `%s`",
-		//	email, address, priceUSD, name, description)
-
-		botLogMessage := fmt.Sprintf("*Email:* `%s`\n*Address:* `%s`\n*Amount:* `%0.2f`\n*Name:* "+
-			"`%s`\n*Product:* `%s`\n*IP Address:* `%s`\n*Country:* `%s`\n*State:* `%s`\n*City:* `%s`\n*Local Time:* `%s`",
-			email, address, priceUSD, name, description, clientIP, ipAPIData.Location.Country, ipAPIData.Location.State, ipAPIData.Location.City, localTime)
-
-		msg := tgbotapi.NewMessage(chatID, botLogMessage)
-		msg.ParseMode = tgbotapi.ModeMarkdown
-		_, err = bot.Send(msg)
-		if err != nil {
-			log.Printf("Error sending message to user: %s", err.Error())
-		}
-		//qrCodeFileName := fmt.Sprintf("%s.png", address)
-		//err = payments.GenerateQRCode(ur, qrCodeFileName)
-		//if err != nil {
-		//	c.JSON(http.StatusInternalServerError, gin.H{
-		//		"message": fmt.Sprintf("Error generating QR code: %v", err.Error()),
-		//	})
-		//	// Add this line to log the actual error message:
-		//	fmt.Println(err)
-		//	return
-		//}
-
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"message": fmt.Sprintf("Error getting Bitcoin price: %s", err.Error()),
-			})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{
-			"address": address,
-			//"qrCodeUrl":   fmt.Sprintf("%s/%s", os.Getenv("QR_CODE_BASE_URL"), qrCodeFileName),
-			"priceInUSD":  priceUSD,
-			"priceInBTC":  priceBTC,
-			"email":       email,
-			"created_at":  utils.GetCurrentTime(),
-			"expired_at":  utils.GetExpiryTime(),
-			"description": description,
-			"name":        name,
-		})
-
-	})
-
-	//payments.QueryBot()
+	r.POST("/cards", handlePayment(bot))
+	r.POST("/usdt", handleUsdtPayment(bot))
+	r.POST("/payment", handlePayment(bot))
 
 	err = r.Run()
 	if err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
+}
+
+func handlePayment(bot *tgbotapi.BotAPI) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		processPaymentRequest(c, bot, true)
+	}
+}
+
+func handleUsdtPayment(bot *tgbotapi.BotAPI) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		processPaymentRequest(c, bot, false)
+	}
+}
+
+func processPaymentRequest(c *gin.Context, bot *tgbotapi.BotAPI, generateAddress bool) {
+	clientIP := c.ClientIP()
+	ipAPIData, err := utils.GetIpLocation(clientIP)
+	if err != nil {
+		log.Printf("Error getting IP location: %s", err)
+	}
+
+	email := c.PostForm("email")
+	priceStr := c.PostForm("price")
+	description := c.PostForm("description")
+	name := c.PostForm("name")
+
+	if email == "" || priceStr == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid input: email and price are required"})
 		return
-	} // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	}
+
+	priceUSD, err := utils.ParseFloat(priceStr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid input: price must be a valid number"})
+		return
+	}
+
+	// Limit address generation
+	session, exists := userSessions[email]
+	if !exists {
+		session = &UserSession{
+			Email:              email,
+			GeneratedAddresses: make(map[string]time.Time),
+		}
+		userSessions[email] = session
+	}
+
+	if len(session.GeneratedAddresses) >= addressLimit {
+		c.JSON(http.StatusTooManyRequests, gin.H{"message": "Address generation limit reached"})
+		return
+	}
+
+	var address string
+	if generateAddress {
+		priceBTC, err := utils.ConvertToBitcoinUSD(priceUSD)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": fmt.Sprintf("Error getting Bitcoin price: %s", err)})
+			return
+		}
+		address, err = payments.GenerateBitcoinAddress(email, priceBTC)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": fmt.Sprintf("Error generating Bitcoin address: %s", err)})
+			return
+		}
+		session.GeneratedAddresses[address] = time.Now()
+	} else {
+		address = "TMm1VE3JhqDiKyMmizSkcUsx4i4LJkfq7G" // Static USDT address for demonstration
+	}
+
+	// Remove expired addresses
+	for addr, createdAt := range session.GeneratedAddresses {
+		if time.Since(createdAt) > addressExpiry {
+			delete(session.GeneratedAddresses, addr)
+		}
+	}
+
+	localTime, err := ipAPIData.ParseLocalTime()
+	if err != nil {
+		log.Printf("Error parsing local time: %s", err)
+	}
+
+	logMessage := fmt.Sprintf("Email: %s, Address: %s, Amount: %.2f, Name: %s, Product: %s", email, address, priceUSD, name, description)
+	log.Printf(logMessage)
+
+	botLogMessage := fmt.Sprintf(
+		"*Email:* `%s`\n*Address:* `%s`\n*Amount:* `%0.2f`\n*Name:* `%s`\n*Product:* `%s`\n*IP Address:* `%s`\n*Country:* `%s`\n*State:* `%s`\n*City:* `%s`\n*Local Time:* `%s`",
+		email, address, priceUSD, name, description, clientIP, ipAPIData.Location.Country, ipAPIData.Location.State, ipAPIData.Location.City, localTime)
+
+	msg := tgbotapi.NewMessage(chatID, botLogMessage)
+	msg.ParseMode = tgbotapi.ModeMarkdown
+	_, err = bot.Send(msg)
+	if err != nil {
+		log.Printf("Error sending message to user: %s", err)
+	}
+
+	responseData := gin.H{
+		"address":     address,
+		"priceInUSD":  priceUSD,
+		"email":       email,
+		"created_at":  utils.GetCurrentTime(),
+		"expired_at":  utils.GetExpiryTime(),
+		"description": description,
+		"name":        name,
+	}
+
+	if generateAddress {
+		priceBTC, err := utils.ConvertToBitcoinUSD(priceUSD)
+		if err == nil {
+			responseData["priceInBTC"] = priceBTC
+		}
+	}
+
+	c.JSON(http.StatusOK, responseData)
 }
