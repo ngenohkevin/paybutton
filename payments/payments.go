d29d19505bd5edc7b42f338767e2a667e4b0be4b
ngenohkevin <ngenohkevin19@gmail.com>
2024-06-12 02:01:05 +0300

    get balance of an address

--- payments/payments.go
+++ payments/payments.go
@@ -1,55 +1,73 @@
 package payments
 
 import (
-	"errors"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
 	"time"
 )
 
-type Payment struct {
-	Email      string
-	PriceUSD   float64
-	PriceBTC   float64
-	Address    string
-	Paid       bool
-	PaidAmount float64
-	Date       time.Time
+type BalanceResponse struct {
+	Address string `json:"address"`
+	Balance int64  `json:"balance"`
 }
 
-var payments []*Payment
+func GetBitcoinAddressBalance(address string) (int64, error) {
+	url := fmt.Sprintf("https://www.blockonomics.co/api/balance")
 
-//func CreatePayment(address string, priceUSD float64, priceBTC float64, email string) (*Payment, error) {
-//	if !utils.IsValidEmail(email) {
-//		return nil, errors.New("invalid email address")
-//	}
-//
-//	if priceUSD <= 0 {
-//		return nil, errors.New("price in USD must be greater than zero")
-//	}
-//
-//	if priceBTC <= 0 {
-//		return nil, errors.New("price in BTC must be greater than zero")
-//	}
-//
-//	payment := &Payment{
-//		Email:      email,
-//		PriceUSD:   priceUSD,
-//		PriceBTC:   priceBTC,
-//		Address:    address,
-//		Paid:       false,
-//		PaidAmount: 0,
-//		Date:       time.Now(),
-//	}
-//
-//	payments = append(payments, payment)
-//
-//	return payment, nil
-//}
-
-func getPaymentByAddress(address string) (*Payment, error) {
-	for _, payment := range payments {
-		if payment.Address == address {
-			return payment, nil
-		}
+	data := map[string]interface{}{
+		"addr": []string{address},
 	}
-	return nil, errors.New("payment not found")
+
+	payload, err := json.Marshal(data)
+	if err != nil {
+		return 0, err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+	if err != nil {
+		return 0, err
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", blockonomicsAPIKey))
+	req.Header.Set("Content-Type", "application/json")
+
+	respChan := make(chan *http.Response)
+	errChan := make(chan error)
+
+	go func() {
+		resp, err := httpClientInstance.client.Do(req)
+		if err != nil {
+			errChan <- err
+			return
+		}
+		respChan <- resp
+	}()
+
+	select {
+	case resp := <-respChan:
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			return 0, fmt.Errorf("error fetching balance, status code: %v", resp.StatusCode)
+		}
+
+		var balanceResponses []BalanceResponse
+		if err := json.NewDecoder(resp.Body).Decode(&balanceResponses); err != nil {
+			return 0, err
+		}
+
+		if len(balanceResponses) == 0 {
+			return 0, fmt.Errorf("no balance data returned")
+		}
+
+		return balanceResponses[0].Balance, nil
+
+	case err := <-errChan:
+		return 0, err
+
+	case <-time.After(time.Second * 30):
+		return 0, fmt.Errorf("timed out waiting for API response")
+	}
 }
