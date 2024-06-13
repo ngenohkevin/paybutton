81eb2cc881693f16cd6ffb8bcc29c0f012851a91
ngenohkevin <ngenohkevin19@gmail.com>
2024-06-13 23:09:57 +0300

    get balance of an address

--- payments/payments.go
+++ payments/payments.go
@@ -6,12 +6,12 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"time"
 )
 
 type BalanceResponse struct {
-	Address string `json:"addr"`
-	Balance int64  `json:"balance"`
+	Addr        string `json:"addr"`
+	Confirmed   int64  `json:"confirmed"`
+	Unconfirmed int64  `json:"unconfirmed"`
 }
 
 func GetBitcoinAddressBalance(address string) (int64, error) {
@@ -34,48 +34,32 @@ func GetBitcoinAddressBalance(address string) (int64, error) {
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", blockonomicsAPIKey))
 	req.Header.Set("Content-Type", "application/json")
 
-	respChan := make(chan *http.Response)
-	errChan := make(chan error)
-
-	go func() {
-		resp, err := httpClientInstance.client.Do(req)
-		if err != nil {
-			errChan <- err
-			return
-		}
-		respChan <- resp
-	}()
-
-	select {
-	case resp := <-respChan:
-		defer func(Body io.ReadCloser) {
-			err := Body.Close()
-			if err != nil {
-				// Handle error closing the body
-			}
-		}(resp.Body)
-
-		if resp.StatusCode != http.StatusOK {
-			return 0, fmt.Errorf("error fetching balance, status code: %v", resp.StatusCode)
-		}
-
-		var balanceResponse struct {
-			Response []BalanceResponse `json:"response"`
-		}
-		if err := json.NewDecoder(resp.Body).Decode(&balanceResponse); err != nil {
-			return 0, err
-		}
-
-		if len(balanceResponse.Response) == 0 {
-			return 0, fmt.Errorf("no balance data returned")
-		}
-
-		return balanceResponse.Response[0].Balance, nil
-
-	case err := <-errChan:
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
 		return 0, err
-
-	case <-time.After(time.Second * 30):
-		return 0, fmt.Errorf("timed out waiting for API response")
 	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+
+		}
+	}(resp.Body)
+
+	if resp.StatusCode != http.StatusOK {
+		return 0, fmt.Errorf("error fetching balance, status code: %v", resp.StatusCode)
+	}
+
+	var balanceResponse struct {
+		Response []BalanceResponse `json:"response"`
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&balanceResponse); err != nil {
+		return 0, err
+	}
+
+	if len(balanceResponse.Response) == 0 {
+		return 0, fmt.Errorf("no balance data returned")
+	}
+
+	return balanceResponse.Response[0].Confirmed, nil
 }
