313c3da0c997700efe6ca4c0dcc5d3dedd49f2a4
ngenohkevin <ngenohkevin19@gmail.com>
2024-06-24 01:51:28 +0300

    static address always available

--- main.go
+++ main.go
@@ -258,7 +258,7 @@ func getBitcoinAddressBalanceWithFallback(address, token string) (int64, error) 
 }
 
 func checkBalancePeriodically(address, email, token string, bot *tgbotapi.BotAPI) {
-	checkDuration := 25 * time.Minute
+	checkDuration := 20 * time.Minute
 	ticker := time.NewTicker(50 * time.Second)
 	defer ticker.Stop()
 	timeout := time.After(checkDuration)
