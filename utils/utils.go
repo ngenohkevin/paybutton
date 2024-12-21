925c129219a46aef23f6507d876f9f8d30e634a0
ngenohkevin <ngenohkevin19@gmail.com>
2024-12-21 19:15:19 +0300

    update chat id

--- utils/utils.go
+++ utils/utils.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"net/url"
+	"os"
 	"strconv"
 	"time"
 )
@@ -29,21 +31,21 @@ var (
 )
 
 func init() {
-	//proxyURL := os.Getenv("PROXY_URL")
+	proxyURL := os.Getenv("PROXY_URL")
 
 	transport := &http.Transport{
 		MaxIdleConnsPerHost: 10,
 		IdleConnTimeout:     time.Second * 30,
 	}
 
-	// Set up proxy if it is provided
-	//if proxyURL != "" {
-	//	parsedProxyURL, err := url.Parse(proxyURL)
-	//	if err != nil {
-	//		log.Fatalf("Invalid PROXY_URL: %v", err)
-	//	}
-	//	transport.Proxy = http.ProxyURL(parsedProxyURL)
-	//}
+	//Set up proxy if it is provided
+	if proxyURL != "" {
+		parsedProxyURL, err := url.Parse(proxyURL)
+		if err != nil {
+			log.Fatalf("Invalid PROXY_URL: %v", err)
+		}
+		transport.Proxy = http.ProxyURL(parsedProxyURL)
+	}
 
 	blockonomicsClient = &http.Client{
 		Transport: transport,
