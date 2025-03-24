fa6f29898505eb9732fb273170c379b9db6cc6d1
ngenohkevin <ngenohkevin19@gmail.com>
2025-03-24 16:27:26 +0300

    refactored and added periodic checking for usdt

---
+++ utils/config.go
@@ -0,0 +1,55 @@
+package utils
+
+import (
+	"github.com/joho/godotenv"
+	"os"
+)
+
+type Config struct {
+	PostgresUser     string
+	PostgresPassword string
+	PostgresDB       string
+	PostgresHost     string
+	BotApiKey        string
+	BlockCypherToken string
+	BlockonomicsAPI  string
+	IpLocation       string
+	MailGunAPIKey    string
+	MailGunPassword  string
+	TronScanAPI      string
+}
+
+func LoadConfig() (config Config, err error) {
+	err = godotenv.Load(".env")
+	if err != nil {
+		return Config{}, err
+	}
+
+	user := os.Getenv("POSTGRES_USER")
+	host := os.Getenv("POSTGRES_HOST")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	db := os.Getenv("POSTGRES_DATABASE")
+	bot := os.Getenv("BOT_API_KEY")
+	blockCypherToken := os.Getenv("BLOCKCYPHER_TOKEN")
+	blockonomicsAPIKey := os.Getenv("BLOCKONOMICS_API_KEY")
+	ipLocation := os.Getenv("IP_LOCATION_API_KEY")
+	mailGunAPI := os.Getenv("MAILGUN_API_KEY")
+	mailGunPass := os.Getenv("MAILGUN_PASSWORD")
+	tronScanAPI := os.Getenv("TRONSCAN_API")
+
+	config = Config{
+		PostgresUser:     user,
+		PostgresPassword: password,
+		PostgresDB:       db,
+		PostgresHost:     host,
+		BotApiKey:        bot,
+		BlockCypherToken: blockCypherToken,
+		BlockonomicsAPI:  blockonomicsAPIKey,
+		IpLocation:       ipLocation,
+		MailGunAPIKey:    mailGunAPI,
+		MailGunPassword:  mailGunPass,
+		TronScanAPI:      tronScanAPI,
+	}
+
+	return config, err
+}
